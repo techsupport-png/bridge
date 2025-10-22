eb30b7757146a3b8f4dd3410eb6e54de4a43d963
Aryan <aryan@bridgebound.com>
2025-10-22 13:00:37 +0530

    sorted

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,8 @@ import type { AppProps } from "next/app";
 import { ClerkProvider } from "@clerk/nextjs";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const publishableKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
-
   return (
-    <ClerkProvider publishableKey={publishableKey}>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}>
       <Component {...pageProps} />
     </ClerkProvider>
   );
